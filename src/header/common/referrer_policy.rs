7303b23193a5776a95f8c7a758e13eda0b2441fd
Raghav <rmuddur@gmail.com>
2016-10-27 19:29:33 -0400

    Updated methods in referrer policy Updated methods to handle strict-origin and strict-origin-when-cross-origin

--- src/header/common/referrer_policy.rs
+++ src/header/common/referrer_policy.rs
@@ -69,6 +69,8 @@ impl Header for ReferrerPolicy {
                 "same-origin" => return Ok(SameOrigin),
                 "origin" => return Ok(Origin),
                 "origin-when-cross-origin" => return Ok(OriginWhenCrossOrigin),
+                "strict-origin" => Ok(StrictOrigin),
+                "strict-origin-when-cross-origin" => Ok(StrictOriginWhenCrossOrigin),
                 "unsafe-url" | "always" => return Ok(UnsafeUrl),
                 _ => continue,
             }
@@ -85,6 +87,8 @@ impl Header for ReferrerPolicy {
             SameOrigin => "same-origin",
             Origin => "origin",
             OriginWhenCrossOrigin => "origin-when-cross-origin",
+            StrictOrigin => "stict-origin",
+            StrictOriginWhenCrossOrigin => "strict-origin-when-cross-origin",
             UnsafeUrl => "unsafe-url",
         })
     }
